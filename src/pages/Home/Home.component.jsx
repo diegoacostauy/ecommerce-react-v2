aebdcaec0f88cd984f9c1901c6ffef62ebdfa1ba
Diego Acosta <diegoacosta211@gmail.com>
2020-11-08 16:09:15 -0300

    change git ignore case

---
+++ src/pages/Home/Home.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.styles.scss';
+import Directory from '../../components/Directory/Directory.component';
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
