c0f90ae999ceb5d7318e8ac9089813709466ec96
Diego Acosta <diegoacosta211@gmail.com>
2020-11-08 17:06:42 -0300

    remove logger from prod env

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV == 'development') {
+  middlewares.push(logger);
+}
 
 //redux devtools extension
 const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
